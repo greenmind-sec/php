8f0d1ae3ba0885869854c19d146375b3fd1d85a8
angelorocha1 <37181292+angelorocha1@users.noreply.github.com>
2019-06-18 22:51:11 -0300

    Separação com slice

--- WinDump/CallPhp.js
+++ WinDump/CallPhp.js
@@ -5,9 +5,8 @@ function ReaderText() {
         data:"",
         success: function (data) {
 
-            var list = data.split(" ");
-            var ordernada = OrdenarLista(list);
-            $('#textList').append(ordernada);
+            var list = data.split("||");
+            $('#textList').append(list);
         }, error: function (jqXhr, textStatus, errorThrown) {
             console.log(errorThrown);
         }
@@ -19,25 +18,4 @@ function ReaderText() {
 
 function StopList() {
   document.getElementById("textList").innerText = "";
-}
-
-function OrdenarLista(list) {
-
-    var listaOrdenada = [];
-    var index = 0;
-    let text = '';
-    let count = 0;
-    for (var x = 0; x <=list.length; x++) {
-        text += ' ' + list[x];
-        index ++;
-        
-        if(index == 7)
-        {
-            count++;
-            listaOrdenada.push(count+ '- ' + text + '</br>');
-            index = 0;
-        }
-    }
-    return listaOrdenada;
-    
 }
--- WinDump/ReaderText.php
+++ WinDump/ReaderText.php
@@ -3,10 +3,9 @@
 $file_handle = fopen("Arquivo.txt", "r");
 while (!feof($file_handle)) {
    $line = fgets($file_handle);
-   echo $line;
+   echo "<p>". $line . "</p></br>||";
 }
 fclose($file_handle);
 
 ?>
-
-<!-- Windump -i 2 -e > C:/xampp/htdocs/WinDump/Arquivo.txt -->
+<!-- Windump -i 2 -c 10 -n > C:/xampp/htdocs/WinDump/Arquivo.txt -->
